2df5977c5f8cc99ad7396b1b72a34e4b7d260b6d
rk0der <ryo.alter@gmail.com>
2016-10-29 21:32:22 +0900

    add macro to print debug info

--- src/util.cu
+++ src/util.cu
@@ -1,29 +1,30 @@
 #include "railgun.h"
 #include <stdio.h>
 
+#define print_header(typename, len)\
+  printf("\ttype: %s\n", typename);\
+  printf("\tlength: %d\n", len);
+#define print_values(format, ptr, len)\
+  printf("\tvalues:\n");\
+  for (int i = 0; i < len; i++) {\
+    printf("\t\t");\
+    printf(format, ptr[i]);\
+    printf("\n");}
+
 void
 dump_args(railgun_args* args)
 {
-  int i;
-
   printf("railgun_args:\n");
   printf("argc: %d\n", args->argc);
-  for (i = 0; i < args->argc; i++) {
+  for (int i = 0; i < args->argc; i++) {
     railgun_data v;
 
     printf("argv[%d]:\n", i);
     v = args->argv[i];
     switch (v.type) {
     case TYPE_INT_P:
-      int *p;
-
-      printf("\ttype: int*\n");
-      printf("\tlength: %d\n", v.n);
-      p = v.d.ip;
-      printf("\tvalues:\n");
-      for (int i = 0; i < v.n; i++) {
-        printf("\t\t%d\n", p[i]);
-      }
+      print_header("int*", v.n);
+      print_values("%d", v.d.ip, v.n);
       break;
     default:
       break;
