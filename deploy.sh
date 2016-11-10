dae87eae02f5e3301c3acb0c417710d224c66a83
rk0der <ryo.alter@gmail.com>
2016-11-10 15:01:14 +0900

    deploy.sh can receive a filename which includes main-function

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
-if [ $# -ne 2 ]; then
-  echo "Usage: ./deploy.sh [REMOTE_HOST] [REMOTE_DIR]"
+if [ $# -ne 3 ]; then
+  echo "Usage: ./deploy.sh [REMOTE_HOST] [REMOTE_DIR] [MAIN_FILE]"
   exit 1;
 fi
 REMOTE_HOST=$1
 REMOTE_DIR=$2
+MAIN_FILE=$3
 
 ssh $REMOTE_HOST "mkdir -p $REMOTE_DIR;"
 fs=`ls -a1 | grep -v -E "^\.git|^\.+$"`
@@ -13,4 +14,4 @@ do
   scp -P 22 -r ./${f} ${REMOTE_HOST}:${REMOTE_DIR}
 done
 
-ssh $REMOTE_HOST "cd $REMOTE_DIR;make clean;make;bin/railgun"
+ssh $REMOTE_HOST "cd $REMOTE_DIR;make clean;make APP=$MAIN_FILE;bin/railgun"
