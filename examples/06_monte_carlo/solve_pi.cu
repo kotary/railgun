7c5f6b503ff049406ba9d7930d2df5ab0c91055f
rk0der <ryo.alter@gmail.com>
2017-01-09 23:53:02 +0900

    new test: solve pi by monte_carlo

---
+++ examples/06_monte_carlo/solve_pi.cu
@@ -0,0 +1,50 @@
+#include "railgun.h"
+
+#include <numeric>
+#include <iostream>
+#include <curand_kernel.h>
+
+__global__
+void monte_pi(int seed, int n_try, float* out) {
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    curandState s;
+
+    curand_init((unsigned int)seed, id, 0, &s);
+
+    int inside = 0;
+    for (int i = 0; i < n_try; ++i) {
+        float x = curand_uniform(&s);
+        float y = curand_uniform(&s);
+        if (x * x + y * y < 1) {
+            inside++;
+        }
+    }
+
+    out[id] = inside / static_cast<float>(n_try);
+}
+
+int main() {
+    int const n_thread = 256;
+    int const n_block = 256;
+    int const n = n_thread * n_block;
+    int const n_try = 1000;
+
+    float *out;
+    cudaHostAlloc((void**)&out, n*sizeof(float), cudaHostAllocDefault);
+
+    railgun_t *rg;
+    railgun_args *args;
+
+    rg = get_railgun();
+    args = rg->wrap_args("II|f", 0, 1, n_try, 1, out, n);
+    rg->schedule((void*)monte_pi, args, n_thread, n_block);
+    rg->execute();
+
+    std::cout
+        << 4.0 * std::accumulate(out, out + n, 0.0) / n
+        << std::endl;
+
+    cudaFreeHost(out);
+
+    return 0;
+}
